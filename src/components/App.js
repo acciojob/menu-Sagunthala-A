ae328a47752dd9449f43c2f19ebb67d14bfa9f0c
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-02-08 11:40:06 +0000

    changing main as id

--- src/components/App.js
+++ src/components/App.js
@@ -198,7 +198,7 @@ const App = ()=>{
     const [dishes,setDishes] = useState('all')
 
   return (
-    <div className='main'>
+    <div id='main'>
     <h1>Our Menu</h1>
     <div className='btn__wrapper'>
         <button>All</button>
